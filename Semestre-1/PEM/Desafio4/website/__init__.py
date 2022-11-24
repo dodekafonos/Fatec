3fc664540d6db396721aafea6ff56d4d0ae736f9
Jonas <jonasalvesbueno@gmail.com>
2022-11-24 10:46:19 -0300

    Sla

--- Semestre-1/PEM/Desafio4/website/__init__.py
+++ Semestre-1/PEM/Desafio4/website/__init__.py
@@ -1,10 +1,6 @@
 from flask import Flask, render_template, url_for
 from .database import create_table_contato, create_database
 
-# Este arquivo transforma a pasta "site" em um módulo.
-# Com isso, qualquer função criada por um arquivo .py
-# pode ser importada.
-
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'mulinhas'
